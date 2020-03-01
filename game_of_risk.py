6286c3eb8e4dc6b3a1be82653c6abfbb7cb01e48
Ryan Krawczyk <ryankrawczyk@Ryans-MacBook.local>
2020-02-29 23:58:09 -0500

    Functionality for mapping/attacking territories

--- game_of_risk.py
+++ game_of_risk.py
@@ -1,3 +1,6 @@
+from random import randint
+
+
 class Player:
     def __init__(self, name):
         self.name = name
@@ -18,46 +21,117 @@ class HumanPlayer(Player):
         self.is_human = True
 
 
+class RiskDeck:
+    def __init__(self, card_count):
+        each_category = card_count // 3
+        self.cards = [1] * each_category + [2] * each_category + [3] * each_category
+        self.card_ceiling = len(self.cards)
+
+    def draw(self):
+        random_card_index = randint(0, len(self.cards) - 1)
+        random_card = self.cards[random_card_index]
+        if random_card_index == len(self.cards) - 1:
+            self.cards = self.cards[:random_card_index]
+        else:
+            self.cards = self.cards[:random_card_index] + self.cards[random_card_index + 1:]
+        return random_card
+
+    def give_back(self, card_list):
+        if len(card_list) != 3:
+            raise Exception('cards can only be returned in sets of 3')
+        if len(self.cards) + len(card_list) > self.card_ceiling:
+            raise Exception('the deck exceeds the {} cards that exist'.format(self.card_ceiling))
+        self.cards.extend(card_list)
+
+
 class Territory:
-    def __init__(self, name, continent, neighbors):
+    def __init__(self, name, continent):
         self.name = name
         self.continent = continent
-        self.neighbors = neighbors
+        self.neighbors = []
         self.occupying_player = None
         self.occupying_armies = 0
 
 
 class GameOfRisk:
+    ARMY_MIN = 20
+    CARD_TRADE_INCREMENT = 2
+    INITIAL_CARD_TRADE = 4
+    PLAYER_MIN = 3
+    PLAYER_MAX = 6
+    TERRITORY_LIMIT = 100
+
     def __init__(self, game_file):
         self.title = ''
         self.players = []
         self.eliminated_players = []
         self.all_territories = []
-        # TODO
-        self.card_deck = None
-        self.total_card_trades = 0
-        # TODO
-        self.dice = None
+        self.armies_for_card_trade = self.INITIAL_CARD_TRADE
+        # Read each line of data file to populate information for game
         with open(game_file, 'r') as f:
             i = 0
             info = f.readline()
             while info:
+                # Number of territories exceeds limit
+                if i > self.TERRITORY_LIMIT + 3:
+                    raise Exception('{} is the maximum number of territories allowed'.format(self.TERRITORY_LIMIT))
+                # First line: title of game
                 if i == 0:
                     self.title = info.strip()
+                # Second line: human players
                 elif i == 1:
                     self.set_players(info)
+                # Third line: computer players
                 elif i == 2:
                     self.set_players(info, is_human=False)
+                # Remaining lines: territory configurations
                 else:
                     self.set_territory(info)
                 info = f.readline()
                 i += 1
             if i < 4:
                 raise Exception('uploaded file does not contain enough information to create a game')
+        if not self.PLAYER_MIN <= len(self.players) <= self.PLAYER_MAX:
+            raise Exception('{} players have been declared but the game requires {} to {}'.format(
+                len(self.players),
+                self.PLAYER_MIN,
+                self.PLAYER_MAX,
+            ))
+        self.card_deck = RiskDeck(len(self.all_territories))
+        self.allocate_armies()
+
+    def allocate_armies(self):
+        # The less players there are, the more armies they receive, at an increment of 5
+        num_armies = self.ARMY_MIN + 5 * (self.PLAYER_MAX - len(self.players))
+        for player in self.players:
+            player.army_count = num_armies
+
+    def attack_territory(self, attacking_army_count, defending_army_count):
+        high_to_low_attack_rolls = self.roll_dice(attacking_army_count)
+        high_to_low_defend_rolls = self.roll_dice(defending_army_count)
+        armies_defeated = 0
+        for i in range(len(high_to_low_defend_rolls)):
+            if high_to_low_attack_rolls[i] > high_to_low_defend_rolls[i]:
+                armies_defeated += 1
+            else:
+                armies_defeated -= 1
+        return armies_defeated
+
+    def get_or_create_territory(self, territory_name, continent_name):
+        for territory in self.all_territories:
+            if territory.name == territory_name:
+                # Continent field updated if territory was first declared as neighbor
+                if continent_name and not territory.continent:
+                    territory.continent = continent_name
+                return territory
+        new_territory = Territory(territory_name, continent_name)
+        self.all_territories.append(new_territory)
+        return new_territory
 
     def set_players(self, line_info, is_human=True):
         player_type = 'human' if is_human else 'computer'
         info_items = line_info.split('|')
+        # First item is number of players, remaining items are player names
         num_players = int(info_items[0].strip())
         i = 1
         try:
@@ -68,12 +142,14 @@ class GameOfRisk:
                     self.players.append(ComputerPlayer(info_items[i].strip()))
                 i += 1
         except IndexError:
+            # Number of players indicated > number of names given
             raise Exception('{} {} players were declared but only {} names were provided'.format(
                 num_players,
                 player_type,
                 i - 1,
             ))
         if i < len(info_items):
+            # Number of players indicated < number of names given
             raise Exception('{} {} players were declared but {} too many names were provided'.format(
                 num_players,
                 player_type,
@@ -82,10 +158,22 @@ class GameOfRisk:
 
     def set_territory(self, line_info):
         info_items = line_info.split('|')
-        neighbor_list = []
         try:
+            current_territory = self.get_or_create_territory(info_items[0].strip(), info_items[1].strip())
+            neighbor_list = []
             for neighbor in info_items[2:]:
-                neighbor_list.append(neighbor.strip())
-            self.all_territories.append(Territory(info_items[0].strip(), info_items[1].strip(), neighbor_list))
+                neighbor_list.append(self.get_or_create_territory(neighbor.strip(), None))
+            current_territory.neighbors.extend(neighbor_list)
         except IndexError:
-            raise Exception('all territories must have at least one neighbor')
+            raise Exception('all territories must belong to a continent and have at least one neighbor')
+
+    @staticmethod
+    def roll_dice(num_rolls):
+        rolls = []
+        for _ in range(num_rolls):
+            current_roll = randint(1, 6)
+            i = 0
+            while i < len(rolls) and current_roll < rolls[i]:
+                i += 1
+            rolls.insert(i, current_roll)
+        return rolls
